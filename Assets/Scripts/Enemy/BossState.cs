caac954ef67d604d5a07f925255dccbad01221d6
2308138 <2308138@student.toa.edu.my>
2025-12-08 22:02:58 +0800

    u38
    - Added BossState.cs

---
+++ Assets/Scripts/Enemy/BossState.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public abstract class BossState : ScriptableObject
+{
+    protected BossController boss;
+
+    public virtual void Initialize(BossController controller) => boss = controller;
+
+    public abstract void EnterState();
+    public abstract void UpdateState();
+    public abstract void ExitState();
+}
