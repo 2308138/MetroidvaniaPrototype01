f6091e5b43cf6132fdf1701bbaef1cea93f95d16
2308138 <2308138@student.toa.edu.my>
2025-12-08 23:34:26 +0800

    u46
    - Added BossState_Death.cs

---
+++ Assets/Scripts/Enemy/Boss/States/BossState_Death.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class BossState_Death : BossState
+{
+    [Header("Death Settings")]
+    public GameObject deathEffectPrefab;
+    public float destroyDelay = 1.2F;
+
+    // --- RUNTIME VARIABLES --- //
+    private bool hasDied = false;
+
+    public override void EnterState()
+    {
+        if (hasDied) return;
+
+        hasDied = true;
+        
+        // --- SPAWN EFFECTS --- //
+        if (deathEffectPrefab != null) GameObject.Instantiate(deathEffectPrefab, boss.transform.position, Quaternion.identity);
+
+        // --- DISABLE COLLIDERS --- //
+        foreach (var col in boss.GetComponents<Collider2D>()) col.enabled = false;
+
+        // --- DISABLE OTHER SCRIPTS --- //
+        foreach (var mb in boss.GetComponents<MonoBehaviour>()) if (mb != boss) mb.enabled = false;
+
+        // --- DISABLE MOVEMENT --- //
+        Rigidbody2D rb = boss.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+            rb.linearVelocity = Vector2.zero;
+        }
+
+        GameObject.Destroy(boss.gameObject, destroyDelay);
+        Debug.Log("Boss has died!");
+    }
+
+    public override void UpdateState() { }
+
+    public override void ExitState() { }
+}
