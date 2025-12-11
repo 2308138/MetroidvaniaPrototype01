db83fdaf90e6740465136af3924d33e79e431c4c
2308138 <2308138@student.toa.edu.my>
2025-12-11 14:18:33 +0800

    u56
    - Added UI_PlayerHealthBar.cs

---
+++ Assets/Scripts/UI/UI_PlayerHealthBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_PlayerHealthBar : MonoBehaviour
+{
+    [Header("UI Hook")]
+    public Image healthFill;
+
+    [Header("Settings")]
+    public float smoothSpeed = 8F;
+
+    // --- RUNTIME VARIABLES --- //
+    private float targetFill = 1F;
+
+    public void SetHealth(float current, float max) => targetFill = current / max;
+
+    private void Update() => healthFill.fillAmount = Mathf.Lerp(healthFill.fillAmount, targetFill, Time.deltaTime * smoothSpeed);
+}
